7cc07537669cd1bf62a04f2afdfe338e2e7b8c6a
Korak Kurani <59537065+Korak-997@users.noreply.github.com>
2024-08-22 14:44:59 +0200

    Create win-pwsh.ps1

---
+++ win-pwsh.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/Korak-997/dotfiles/main/oh-my-posh.json" | Invoke-Expression
+
+function gush {
+    $commitMessage = $args[0]
+    git add .
+    git commit -m "$commitMessage"
+    git push
+}
